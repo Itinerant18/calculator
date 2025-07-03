216d04785c397b540232170ca6d9230338b6dc2f
User Defined <userdefined2209@gmail.com>
2025-07-03 06:44:53 +0000

    Warning: Extra attributes from the server: data-new-gr-c-s-check-loaded,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" integrity="sha384-n8MVd4RsNIU0KOV3CQDEZbtmK7TFNSPdaGMryiDNf07UA4/gxS699iReEfAud8iF" crossOrigin="anonymous" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
